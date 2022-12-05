ea505e0bb71c7312e3427f03e136e191b10f2059
ambika-devi <ambikadevithota5@gmail.com>
2022-12-05 20:55:04 +0530

    get products using axios request

--- store.js
+++ store.js
@@ -1,71 +1,136 @@
-const openCart = document.querySelector(".open-cart");
-const closeCart = document.querySelector(".close-cart");
-const cart = document.querySelector(".cart");
-const cartBody = document.querySelector(".cart-body ul");
-const body = document.querySelector("body");
 
-const findTotal = () => {
-  const cartItems = document.querySelectorAll(".cart-item");
-  let total = 0;
-  for (let i = 0; i < cartItems.length; i++) {
-    const price = cartItems[i].querySelector(".item-price").textContent;
-    const quantity =
-      cartItems[i].querySelector(".item-quantity").children[0].value;
-    total = total + price * quantity;
-  }
-  document.querySelector(".cart-total").textContent = `Total ($) : ${total}`;
-};
-
-const updateTotal = (e) => {
-  const quantity = document.querySelectorAll(".quantity");
-  for (let i = 0; i < quantity.length; i++) {
-    if (quantity[i].value <= 0) {
-      quantity[i].value = 1;
-    }
-  }
-  findTotal();
-};
-
-const showCart = () => {
-  console.log("clicked");
-  cart.style.visibility = "visible";
-};
-const hideCart = () => {
-  cart.style.visibility = "hidden";
-};
-
-const addToCart = (e) => {
-  if (e.target.tagName == "BUTTON") {
-    const parent = e.target.parentElement.parentElement;
-    const itemName = parent.querySelector(".img-heading").textContent;
-    const itemPrice = parent
-      .querySelector(".img-price")
-      .textContent.replace("$", "");
-    const itemQuantity = 1;
-
-    const itemRow = document.createElement("div");
-    itemRow.innerHTML = `<li class="item-name">${itemName}</li>
-    <li class="item-price">${itemPrice}</li>
-    <li class="item-quantity">
-      <input type="number" class="quantity" value="1" />
-    </li>`;
-    itemRow.className = "cart-item";
-
-    const cartTable = document.querySelector(".cart-items");
-    cartTable.insertAdjacentElement("beforeend", itemRow);
-
-    //creating toast message or snackbar
-    const toast = document.createElement("div");
-    toast.innerText = "Product added to cart";
-    toast.className = "toast";
-    body.insertAdjacentElement("beforeend", toast);
-
-    setTimeout(() => {
-      toast.remove();
-    }, 3000);
-  }
-  //calculate total after adding new element to cart
-  findTotal();
-};
-openCart.addEventListener("click", showCart);
-closeCart.addEventListener("click", hideCart);
+      const openCart = document.querySelector(".open-cart");
+      const closeCart = document.querySelector(".close-cart");
+      const cart = document.querySelector(".cart-container");
+      const cartBody = document.querySelector(".cart-body ul");
+      const body = document.querySelector("body");
+      
+      const findTotal = () => {
+        const cartItems = document.querySelectorAll(".cart-item");
+        let total = 0;
+        for (let i = 0; i < cartItems.length; i++) {
+          const price = cartItems[i].querySelector(".item-price").textContent;
+          const quantity =
+            cartItems[i].querySelector(".item-quantity").children[0].value;
+          total = total + price * quantity;
+        }
+        document.querySelector(".cart-total").textContent = `Total ($) : ${total}`;
+      };
+      const updateTotal = (e) => {
+        const quantity = document.querySelectorAll(".quantity");
+        for (let i = 0; i < quantity.length; i++) {
+          if (quantity[i].value <= 0) {
+            quantity[i].value = 1;
+          }
+        }
+        findTotal();
+      };
+      
+      const showCart = () => {
+        // console.log("clicked");
+        const cartBackground = document.querySelector(".cart");
+        // cartBackground.style.visibility = "visible";
+        findTotal();
+        cart.style.visibility = "visible";
+      };
+      const hideCart = () => {
+        const cartBackground = document.querySelector(".cart");
+        // cartBackground.style.visibility = "hidden";
+        cart.style.visibility = "hidden";
+      };
+      const deleteCartItem = (e) => {
+        e.preventDefault();
+        if (e.target.className == "delete-item") {
+          e.target.parentElement.parentElement.parentElement.remove();
+          findTotal();
+          createToast("Item deleted from Cart");
+        }
+      };
+      const createToast = (msg) => {
+        const toast = document.createElement("div");
+        toast.innerText = msg;
+        toast.className = "toast";
+        body.insertAdjacentElement("beforeend", toast);
+      
+        setTimeout(() => {
+          toast.remove();
+        }, 3000);
+      };
+      
+      const checkIfItemExistsInCart = (itemName) => {
+        const cartItems = document.querySelectorAll(".cart-item");
+        for (let i = 0; i < cartItems.length; i++) {
+          const name = cartItems[i].querySelector(".item-name").textContent;
+          if (name == itemName) {
+            return true;
+          }
+        }
+        return false;
+      };
+      
+      const addToCart = (e) => {
+        if (e.target.tagName == "BUTTON") {
+          const parent = e.target.parentElement.parentElement;
+          const itemName = parent.querySelector(".img-heading").textContent;
+          const itemPrice = parent
+            .querySelector(".img-price")
+            .textContent.replace("$", "");
+          const itemQuantity = 1;
+      
+          //check if it is in the cart
+          const itemExists = checkIfItemExistsInCart(itemName);
+          if (itemExists) {
+            return createToast("Product already present in the cart");
+          } else {
+            const itemRow = document.createElement("div");
+            itemRow.innerHTML = `<li class="item-name">${itemName}</li>
+          <li class="item-price">${itemPrice}</li>
+          <li class="item-quantity">
+            <input type="number" class="quantity" value="1" />
+            <span><button class="delete-item">✕</button></span>
+          </li>`;
+            itemRow.className = "cart-item";
+      
+            const cartTable = document.querySelector(".cart-items");
+            cartTable.insertAdjacentElement("beforeend", itemRow);
+      
+            //creating toast message
+            createToast("Product added to Cart");
+            //calculate total after adding new element
+            findTotal();
+          }
+        }
+      };
+    const displayProduct = ({ id, title, imageUrl, price }) => {
+      // console.log(id, title, imageUrl, price);
+      const imgContainer = document.querySelector(".img-container.music");
+      console.log(imgContainer);
+      const img = document.createElement("div");
+      img.className = "img";
+      img.innerHTML = ` <h3 class="img-heading">${title}</h3>
+      <img
+        src=${imageUrl}
+        alt="product image"
+      />
+      <div class="img-action">
+        <p class="img-price">$${price}</p>
+        <button id="${id}">Add to Cart</button>
+      </div>`;
+      imgContainer.insertAdjacentElement("beforeend", img);
+    };
+    
+    const getProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/products");
+        const products = response.data.products;
+        console.log(products);
+        products.forEach((product) => {
+          displayProduct(product);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+      openCart.addEventListener("click", showCart);
+      closeCart.addEventListener("click", hideCart);
+      document.addEventListener("DOMContentLoaded",getProducts);
